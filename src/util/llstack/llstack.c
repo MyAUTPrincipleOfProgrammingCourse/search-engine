99cd0c6b1f7e976bf3ab66a7f4790f876da0e7c0
Ahmad Anvari <ahmad.anv@gmail.com>
2018-01-10 23:31:09 +0330

    Adding simple stack

---
+++ src/util/llstack/llstack.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <memory.h>
+#include "llstack.h"
+
+void llstack_init(LLStack *llstack)
+{
+    (*llstack) = malloc(sizeof(LLStackType));
+    (*llstack)->__start_node = (*llstack)->__last_node = NULL;
+    (*llstack)->size = 0;
+}
+
+void llstack_push(LLStack llstack, LLStackData llstack_data)
+{
+    if (llstack->__start_node == NULL)          // Stack is empty
+    {
+        LLStackNode node = malloc(sizeof(LLStackNodeType));
+        node->__data = llstack_data;
+        node->__ll_last = NULL;
+        llstack->__start_node = llstack->__last_node = node;
+    }
+    else
+    {
+        LLStackNode node = malloc(sizeof(LLStackNodeType));
+        node->__data = llstack_data;
+        node->__ll_last = llstack->__last_node;
+        llstack->__last_node = node;
+    }
+
+    llstack->size ++;
+}
+
+void llstack_pop(LLStack llstack)
+{
+    if (llstack->__start_node != NULL)
+    {
+        if (llstack->__last_node == llstack->__start_node)
+        {
+            free(llstack->__last_node);
+            llstack->__start_node = llstack->__last_node = NULL;
+        }
+        else
+        {
+            LLStackNode last_node = llstack->__last_node->__ll_last;
+            free(llstack->__last_node);
+            llstack->__last_node = last_node;
+        }
+
+        llstack->size --;
+    }
+}
+
+LLStackData llstack_top(LLStack llstack)
+{
+    if (llstack->size == 0)
+        return NULL;
+    return llstack->__last_node->__data;
+}
+
+LLStackData llstack_top_pop(LLStack llstack)
+{
+    LLStackData data = llstack_top(llstack);
+    if (data) llstack_pop(llstack);
+    return data;
+}
+
+void llstack_release(LLStack llstack)
+{
+    // TODO implement this part
+}
+
+bool llstack_is_empty(LLStack llstack) {
+    return ((llstack->__last_node == llstack->__start_node) && (llstack->__last_node == NULL)) || (llstack->size == 0);
+}
