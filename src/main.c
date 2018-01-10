68b133e4e4ede7fe9333ab4854bb5dd1ce7632fa
Ahmad Anvari <ahmad.anv@gmail.com>
2018-01-10 23:32:35 +0330

    Adding a simple main file

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "parse/parse.h"
+
+int main(int argc, char **argv)
+{
+
+    scan_dir("/home/ahmad/CLionProjects/search-engine/resource/input");
+//    show_dir_content("/home/ahmad/CLionProjects/search-engine");
+
+//    printf("%s\n", NORMAL_COLOR);
+
+
+    return(0);
+}
