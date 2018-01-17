179b738502b7da78c7a7aaa9255094b84fdcfb33
Ahmad Anvari <ahmad.anv@gmail.com>
2018-01-18 01:05:31 +0330

    Making the main file more clean and add comments

--- src/main.c
+++ src/main.c
@@ -1,17 +1,26 @@
 #include "parse/parse.h"
-#include "bst/bst.h"
 #include "query/query.h"
 #include <stdio.h>
+#include "collection/word.h"
 
 int main(int argc, char **argv)
 {
-    load_error_check_automata();
+    load_error_check_automata();        // Load the automata table from the file "resource/automata-table"
 
-    bst_init();
-    scan_stop_words("/home/ahmad/CLionProjects/search-engine/resource/input/stopwords.txt");
-    scan_dir("/home/ahmad/CLionProjects/search-engine/resource/input");
-    bst_inorder();
-    start_query_get();
+    char *files_path = "/home/ahmad/CLionProjects/search-engine/resource/input/content"; // Path of the directory of the files
+    char *stop_words_path = "../resource/input/stopwords.txt";                           // Path of the stop words file
 
-    return 0;
+    init_stop_word_collection(stop_words_path);                                          // Init the bst of stop words
+    init_word_collection(files_path);                                                    // Init the bst of words
+
+    // Printing the collection
+    printf("Stop words:\n");
+    print_stop_word_collection();                                                        // Printing the stop words
+
+    printf("Stop words:\n");
+    print_word_collection();                                                             // Printing the words
+
+    start_query_get();                                                                   // Start getting the query from the user
+
+    return EXIT_SUCCESS;
 }
